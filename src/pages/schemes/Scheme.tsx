ad8d944cdad5892be5de046f8726370eb121ca42
Lucas <lucassoares2007@hotmail.com>
2023-05-19 12:04:08 -0300

    refactor: scheme

--- src/pages/schemes/Scheme.tsx
+++ src/pages/schemes/Scheme.tsx
@@ -1,10 +1,4 @@
-import {
-  PersonAdd,
-  Class,
-  HowToReg,
-  PeopleAlt,
-  AddTask,
-} from "@mui/icons-material";
+import { Class, AddTask } from "@mui/icons-material";
 import { Card, CardContent, Typography } from "@mui/material";
 import { BasePage } from "../../shared/components";
 import { Link } from "react-router-dom";
@@ -43,57 +37,6 @@ export const Scheme = () => {
           <CardContent sx={{ display: "flex", gap: 2 }}>
             <Class />
             <Typography>Iniciar Plano</Typography>
-          </CardContent>
-        </Link>
-      </Card>
-      <Card
-        sx={{
-          width: "100%",
-          height: 80,
-          maxWidth: 250,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Link to="/user/create">
-          <CardContent sx={{ display: "flex", gap: 2 }}>
-            <PersonAdd />
-            <Typography>Adicionar Usuário</Typography>
-          </CardContent>
-        </Link>
-      </Card>
-      <Card
-        sx={{
-          width: "100%",
-          maxWidth: 250,
-          height: 80,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Link to="/user/accept">
-          <CardContent sx={{ display: "flex", gap: 2 }}>
-            <HowToReg />
-            <Typography>Aceitar Usuário</Typography>
-          </CardContent>
-        </Link>
-      </Card>
-      <Card
-        sx={{
-          width: "100%",
-          maxWidth: 250,
-          height: 80,
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Link to="/user/list">
-          <CardContent sx={{ display: "flex", gap: 2 }}>
-            <PeopleAlt />
-            <Typography>Listar Usuários</Typography>
           </CardContent>
         </Link>
       </Card>
